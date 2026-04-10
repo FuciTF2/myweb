6f4d5124bf286c88ec7b181aa10f2a5cbf8e1620
WillDevBrook <nypljan51@gmail.com>
2026-04-10 23:28:18 +0200

    more secret stuff

--- portfolio/server.js
+++ portfolio/server.js
@@ -56,6 +56,7 @@ app.get("/portfolio",  (req, res) => res.sendFile(path.join(__dirname, "public",
 app.get("/playground", (req, res) => res.sendFile(path.join(__dirname, "public", "playground.html")));
 app.get("/game",       (req, res) => res.sendFile(path.join(__dirname, "public", "game.html")));
 app.get("/lore",       (req, res) => res.sendFile(path.join(__dirname, "public", "lore.html")));
+app.get("/william",    (req, res) => res.sendFile(path.join(__dirname, "public", "william.html")));
 app.get("/chatbot",    (req, res) => res.sendFile(path.join(__dirname, "public", "chatbot.html")));
 
 app.listen(PORT, () => {
